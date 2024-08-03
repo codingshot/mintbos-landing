ceb8877facd6c1255b008667730d07df68460cc6
codingshot <45281667+codingshot@users.noreply.github.com>
2024-08-03 11:23:40 -0700

    Update [redirect].js there was a coma error

--- pages/[redirect].js
+++ pages/[redirect].js
@@ -13,7 +13,7 @@ const RedirectLinks = {
   "/twitter": "https://x.com/genadrop",
   "/telegram": "https://t.me/GenaDrop",
   "/wallet": "https://wallet.genadrop.xyz",
-  "/support": "https://t.me/genadrop"
+  "/support": "https://t.me/genadrop",
   "/mintbase-figma": "https://www.figma.com/file/PMmovNI2wfyjVrJThDu0HB/MINTBASE-OPEN?type=design&node-id=0-1&mode=design&t=VWEnFpgaA9TDHkVY-0",
 
 };
